26a3c786fb65a7681e7cdbb364e18ebe4aface3c
Your Name <you@example.com>
2024-03-03 10:05:45 +0530

    htmlContent

--- index.php
+++ index.php
@@ -51,13 +51,13 @@ if(isset($_POST['name'])){
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <img class="bg"  alt="IIT Kharagpur">
+   
     <div class="container">
-        <h1>Welcome to IIT Kharagpur US Trip form</h3>
+        <h1>Welcome to Industrial Visit Trip form</h3>
         <p>Enter your details and submit this form to confirm your participation in the trip </p>
         <?php
         if($insert == true){
-        echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the US trip</p>";
+        echo "<p class='submitMsg'>Thanks for submitting your form. We are happy to see you joining us for the  trip</p>";
         }
     ?>
         <form action="index.php" method="post">
